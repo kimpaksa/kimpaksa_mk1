8f0740b5974bb1632667b222d55bf8d915b99dc8
kimpaksa <kimpaksayosi@naver.com>
2014-04-04 12:13:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,32 @@
   	//$keychar = substr($key, ($i % strlen($key))-1, 1);
   	
   	//$char = chr(ord($char)+ord($keychar));
+  	
+  	
+  /* 공부해야할 부분 2014.04.04
+  	function encrypt($string, $key) {
+  $result = '';
+  for($i=0; $i<strlen($string); $i++) {
+    $char = substr($string, $i, 1);
+    $keychar = substr($key, ($i % strlen($key))-1, 1);
+    $char = chr(ord($char)+ord($keychar));
+    $result.=$char;
+  }
+  return base64_encode($result);
+}
+function decrypt($string, $key) {
+  $result = '';
+  $string = base64_decode($string);
+  for($i=0; $i<strlen($string); $i++) {
+    $char = substr($string, $i, 1);
+    $keychar = substr($key, ($i % strlen($key))-1, 1);
+    $char = chr(ord($char)-ord($keychar));
+    $result.=$char;
+  }
+  return $result;
+}
+  */
+  
   
 ?>
 <html>
